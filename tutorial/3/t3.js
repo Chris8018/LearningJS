1800948090c0abda58338242e1dd39e0d3dfde41
Chris8018 <chris@chrisu16.lan>
2018-11-23 14:04:27 +0000

    Add tutorial 3

---
+++ tutorial/3/t3.js
@@ -0,0 +1,13 @@
+document.write("Math.E = ", Math.E, "<br/>");
+document.write("Math.PI = ", Math.PI, "<br/>");
+document.write("Math.abs(-1) = ", Math.abs(-1), "<br/>");
+document.write("Math.cbrt(1000) = ", Math.cbrt(1000), "<br/>");
+document.write("Math.ceil(6.45) = ", Math.ceil(6.45), "<br/>");
+document.write("Math.floor(6.45) = ", Math.floor(6.45), "<br/>");
+document.write("Math.round(6.45) = ", Math.round(6.45), "<br/>");
+document.write("Math.log(10) = ", Math.log(10), "<br/>");
+document.write("Math.log10(10) = ", Math.log10(10), "<br/>");
+document.write("Math.max(100, 23) = ", Math.max(100, 23), "<br/>");
+document.write("Math.min(-49, 40) = ", Math.min(-49, 40), "<br/>");
+document.write("Math.pow(4, 2) = ", Math.pow(4, 2), "<br/>");
+document.write("Math.sqrt(1000) = ", Math.sqrt(1000), "<br/>");
