5e51a6756716fed87ff23a141c35fb3a85d31416
Chris8018 <chris@chrisu16.lan>
2018-11-23 13:46:38 +0000

    Update tutorial 2

--- tutorial/2/t2.js
+++ tutorial/2/t2.js
@@ -1,2 +1,20 @@
+document.write("<p>Task 1</p>");
 document.write("10 + 5 = ", 10 + 5, "<br/>");
 document.write("10 + 5 = " + 10 + 5, "<br/>");
+
+document.write("<p>Task 2</p>");
+document.write("10 + 5 = ", 10 + 5, "<br/>");
+document.write("10 - 5 = ", 10 - 5, "<br/>");
+document.write("10 * 5 = ", 10 * 5, "<br/>");
+document.write("10 / 5 = ", 10 / 5, "<br/>");
+document.write("10 % 5 = ", 10 % 5, "<br/>");
+
+document.write("<p>Task 3</p>");
+document.write("10 / 3 = ", 10 / 3, "<br/>");
+document.write("round (10 / 3) =", (10 / 3).toFixed(2), "<br/>");
+
+document.write("<p>Task 4</p>");
+var fNum = 10;
+document.write("fnum = ", fNum, "<br/>");
+document.write("fnum++ = ", fNum++, "<br/>");
+document.write("++fnum = ", ++fNum, "<br/>");
