60b76843c16300229fd14a7fcf8c276f20c9e177
Chris8018 <chris@chrisu16.lan>
2018-11-23 14:35:03 +0000

    Update tutorial 3

--- tutorial/3/t3.js
+++ tutorial/3/t3.js
@@ -1,13 +1,22 @@
+document.write("<p>Task 1</p>");
 document.write("Math.E = ", Math.E, "<br/>");
 document.write("Math.PI = ", Math.PI, "<br/>");
 document.write("Math.abs(-1) = ", Math.abs(-1), "<br/>");
 document.write("Math.cbrt(1000) = ", Math.cbrt(1000), "<br/>");
 document.write("Math.ceil(6.45) = ", Math.ceil(6.45), "<br/>");
-document.write("Math.floor(6.45) = ", Math.floor(6.45), "<br/>");
-document.write("Math.round(6.45) = ", Math.round(6.45), "<br/>");
+document.write("Math.floor(6.45) = ", Math.floor(16.55), "<br/>");
+document.write("Math.round(6.45) = ", Math.round(16.55), "<br/>");
 document.write("Math.log(10) = ", Math.log(10), "<br/>");
 document.write("Math.log10(10) = ", Math.log10(10), "<br/>");
 document.write("Math.max(100, 23) = ", Math.max(100, 23), "<br/>");
 document.write("Math.min(-49, 40) = ", Math.min(-49, 40), "<br/>");
 document.write("Math.pow(4, 2) = ", Math.pow(4, 2), "<br/>");
 document.write("Math.sqrt(1000) = ", Math.sqrt(1000), "<br/>");
+
+document.write("<p>Task 2</p>");
+document.write("Random # (1-10) = ", Math.floor(Math.random() * 10 + 1), "<br/>");
+
+document.write("<p>Task 3</p>");
+document.write("Convert String = ", Number("3.4"), "<br/>");
+document.write("Convert Int = ", parseInt("4.3"), "<br/>");
+document.write("Convert Float = ", parseFloat("4.3"), "<br/>");
