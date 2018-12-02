12f2b83cbdbe256c69f1052e1edc96d5b0e538c5
Chris8018 <tranuytrieuvi@gmail.com>
2018-12-02 14:14:07 +0000

    Update t14.js

--- tutorial/14/t14.js
+++ tutorial/14/t14.js
@@ -1,13 +1,16 @@
-var tStr = "Try styling this string!";
+let index = 2;
+var custA = ["Tom", "Bob", "Hello"];
 
-document.write("Big: ", tStr.big(), "<br/>");
-document.write("Bold: ", tStr.bold(), "<br/>");
-document.write("Font Color: ", tStr.fontcolor("red"), "<br/>");
-document.write("Font Size: ", tStr.fontsize("32px"), "<br/>");
-document.write("Italics: ", tStr.italics(), "<br/>");
-document.write("Google: ", tStr
-    .link("https://www.w3schools.com/tags/ref_pxtoemconversion.asp"), "<br/>");
-document.write("Small: ", tStr.small(), "<br/>");
-document.write("Strike: ", tStr.strike(), "<br/>");
-document.write("Sub: ", tStr.sub(), "<br/>");
-document.write("Sup: ", tStr.sup(), "<br/>");
+var getCust = i => {
+    if (i >= custA.length)
+        throw new RangeError("OutOfBoundException")
+    else
+        return custA[i];
+}
+
+try {
+    console.log(getCust(index));
+} catch (ex) {
+    if (ex instanceof RangeError)
+        console.log(ex.message);
+}
