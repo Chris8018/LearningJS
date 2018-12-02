68dfe7b2e85e3b46db23de8fcb4a5b18117f4e72
Chris8018 <tranuytrieuvi@gmail.com>
2018-12-02 11:39:42 +0000

    Add tutorial 14

---
+++ tutorial/14/t14.js
@@ -0,0 +1,13 @@
+var tStr = "Try styling this string!";
+
+document.write("Big: ", tStr.big(), "<br/>");
+document.write("Bold: ", tStr.bold(), "<br/>");
+document.write("Font Color: ", tStr.fontcolor("red"), "<br/>");
+document.write("Font Size: ", tStr.fontsize("32px"), "<br/>");
+document.write("Italics: ", tStr.italics(), "<br/>");
+document.write("Google: ", tStr
+    .link("https://www.w3schools.com/tags/ref_pxtoemconversion.asp"), "<br/>");
+document.write("Small: ", tStr.small(), "<br/>");
+document.write("Strike: ", tStr.strike(), "<br/>");
+document.write("Sub: ", tStr.sub(), "<br/>");
+document.write("Sup: ", tStr.sup(), "<br/>");
